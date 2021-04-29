44baebe4931a8d5f99043714aab6fa1a204ba8eb
samsul <samsuladry@gmail.com>
2021-04-29 16:26:18 +0800

    first commit

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quasi alias laboriosam cum. Similique nulla quam a quos illum minima facere aut ab voluptatem veniam, commodi quas aliquid optio? Harum?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quasi alias laboriosam cum. Similique nulla quam a quos illum minima facere aut ab voluptatem veniam, commodi quas aliquid optio? Harum?</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
